0005952c9a5167aff40d3239e493c80d601e24ca
Franco De BAttista <cpn.francodebattista@gmail.com>
2020-11-03 16:14:44 -0300

    DBA-0008: update checks and make the button

--- js/subscription.js
+++ js/subscription.js
@@ -1,50 +1,37 @@
 
 
-    var space =" ";
+
 window.onload = function() {
     // ***** check full name *****
-   /* var inputName = document.getElementById("new-name");
-    inputName.addEventListener('blur', checkName);
-    inputName.addEventListener('blur', checkSpaceName);
+    var inputName = document.getElementById("new-name");
+    inputName.onblur = checkName;
     function checkName (a) {
         a.preventDefault;
         var name = inputName.value;
-        if(name.length < 6){
-            return document.getElementById("nameError").classList.remove("no-error");
-        }
-        return true
-    }
-    function checkSpaceName () {
-        var name =inputName.value;
         var myRegex = /\s/;
         var result = myRegex.test(name);
+        if(name.length < 6){
+            return document.getElementById("nameError").classList.remove("no-error");
+        }
         if (result != true) {
             return document.getElementById("nameError").classList.remove("no-error");
         }
     }
-    inputName.addEventListener('focus', checkNameFocus);
-    inputName.addEventListener('focus', checkSpaceNameFocus);
+    inputName.onfocus = checkNameFocus
     function checkNameFocus (a) {
-        a.preventDefault
         var name = inputName.value;
+        var myRegex = /\s/
+        var result = myRegex.test(name);
+
         if(name.length < 6){
             return document.getElementById("nameError").classList.add("no-error")
         }
-        return true
-    }
-    function checkSpaceNameFocus () {
-        var name =inputName.value
-        var myRegex = /\s/
-        var result = myRegex.test(name);
         if (result != true) {
             return document.getElementById("nameError").classList.add("no-error")
-        } else {
-        }
+        }  
     }
-*/
 
     // ***** check email *****
-    // VARIABLE A CAMBIAR email
     var inputEmail = document.getElementById("email");
     inputEmail.addEventListener("blur",checkEmailName)
     inputEmail.addEventListener("blur",checkMailArroba)
@@ -94,9 +81,8 @@ window.onload = function() {
     }
 
     // ***** check pasword *****
-    // VARIABLE to change pasword
     var inputPassword = document.getElementById("password");
-    inputPassword.addEventListener("blur",passwordCheckNumbers)
+    inputPassword.onblur = passwordCheckNumbers;
     function passwordCheckNumbers(a){
         var password = inputPassword.value;
         var myRegex = /[0-9]/;
@@ -118,8 +104,7 @@ window.onload = function() {
             return document.getElementById("passwordError").classList.remove("no-error")        
         }
     }
-
-    inputPassword.addEventListener("focus",passwordCheckNumbersOne)
+    inputPassword.onfocus = passwordCheckNumbersOne;
     function passwordCheckNumbersOne(a){
         var password = inputPassword.value;
         var myRegex = /[0-9]/;
@@ -143,9 +128,8 @@ window.onload = function() {
     }
 
     // ***** check repeatPasword *****
-    // VARIABLE to change repeatPasword
     var inputRepeatPassword = document.getElementById("repeatPassword")
-    inputRepeatPassword.addEventListener("blur",checkPassword)
+    inputRepeatPassword.onblur = checkPassword
     function checkPassword (a) {
         var password = inputPassword.value;
         var RepeatPassword = inputRepeatPassword.value;
@@ -153,7 +137,7 @@ window.onload = function() {
             return document.getElementById("repeatPasswordError").classList.remove("no-error")         
         }
     }
-    inputRepeatPassword.addEventListener("focus",checkPasswordOne)
+    inputRepeatPassword.onfocus = checkPasswordOne
     function checkPasswordOne (a) {
         var password = inputPassword.value;
         var RepeatPassword = inputRepeatPassword.value
@@ -162,114 +146,183 @@ window.onload = function() {
         }
     }
 
-}
-/*
-    // ***** check Age *****
-    // VARIABLE to change Age
-    var age = 21;
 
-    function checkAge (a){
-        if(a<18){
-            return "Age: <18";
+
+    // ***** check Age *****
+    var inputAge = document.getElementById("age");
+    inputAge.onblur = checkAge;
+    function checkAge (){
+        age = inputAge.value;
+        if(age<18){
+            return document.getElementById("ageError").classList.remove("no-error")         
         }
-        if (Number.isInteger(a)){
-            return "Age: ok";
+        if (age % 1 != 0) {
+            return document.getElementById("ageError").classList.remove("no-error")         
         }
-        return "Age: is not integer";
     }
-    console.log(checkAge(age))
+    inputAge.onfocus = checkAgeOne
+    function checkAgeOne (){
+        age = inputAge.value;
+        if(age<18){
+            return document.getElementById("ageError").classList.add("no-error")         
+        }
+        if (age % 1 != 0) {
+            return document.getElementById("ageError").classList.add("no-error")         
+        }
+    }
+
 
     // ***** check Phone Number *****
-    // VARIABLE to PhoneNumber
-    var phoneNumber = "451 7367";
-
+    var inputPhoneNumber = document.getElementById("phoneNumber");
+    inputPhoneNumber.onblur = checkhPhoneNumber
     function checkhPhoneNumber (a) {
-        if(a.length<7){
-            return "Phone Number: < 7 number"
+        var phoneNumber = inputPhoneNumber.value
+        var myregexSimbol = /\W|[a-z]/ig;
+        var result = myregexSimbol.test(phoneNumber)
+        if(phoneNumber.length<7){
+            return document.getElementById("phoneNumberError").classList.remove("no-error")         
         }
-        var simbol="()[]{}-_) !·$%&/=?¿^*¨ç|@#¬¡º";
-        for(i=0; i<a.length; i++){
-            if (simbol.indexOf(a.charAt(i),0)!=-1){
-            return "Phone Number: Have simbols";
-            }
-        }
-        return "Phone Number:Ok";
+       if(result === true){
+        return document.getElementById("phoneNumberError").classList.remove("no-error")         
+       }
+       
     }
-    console.log(checkhPhoneNumber(phoneNumber))
+    inputPhoneNumber.onfocus = checkhPhoneNumberOne
+    function checkhPhoneNumberOne (a) {
+        var phoneNumber = inputPhoneNumber.value
+        var myregexSimbol = /\W|[a-z]/ig;
+        var result = myregexSimbol.test(phoneNumber)
+        if(phoneNumber.length<7){
+            return document.getElementById("phoneNumberError").classList.add("no-error")         
+        }
+       if(result === true){
+        return document.getElementById("phoneNumberError").classList.add("no-error")         
+       }       
+    }
 
     // ***** check Address *****
-    // VARIABLE to Address
-    var address = "Dorrego1 ";
-
-    function checkAddressNumbers(a){
-    for(i=0; i<a.length; i++){
-        if (numbers.indexOf(a.charAt(i),0)!=-1){
-            return "Address: Have Numbers";
+    var inputAddress = document.getElementById("address");
+    inputAddress.onblur = checkAddres
+    function checkAddres(){
+        var address = inputAddress.value;
+        var myRegex = /[0-9]/ig;
+        var myRegexOne = /[a-z]/ig;
+        var myRegexTwo = /\s/;
+        var result = myRegex.test(address);
+        var resultOne = myRegexOne.test(address);
+        var resultTwo = myRegexTwo.test(address)
+        if (address.length < 5){
+            return document.getElementById("addressError").classList.remove("no-error")
+        }
+        if (result != true) {
+            return document.getElementById("addressError").classList.remove("no-error")        
+        }
+        if (resultOne != true) {
+            return document.getElementById("addressError").classList.remove("no-error")        
+        }
+        if (resultTwo != true) {
+            return document.getElementById("addressError").classList.remove("no-error")        
         }
     }
-    return "Address: Have no numbers";
-    }
-    console.log(checkAddressNumbers(address))
-
-    function checkAddressLetters(a){
-        a = a.toLowerCase()
-    for(i=0; i<a.length; i++){
-        if (letters.indexOf(a.charAt(i),0)!=-1){
-            return "Address: Have letters";
+    inputAddress.onfocus = checkAddresOne
+    function checkAddresOne(){
+        var address = inputAddress.value;
+        var myRegex = /[0-9]/;
+        var myRegexOne = /[a-z]/ig;
+        var myRegexTwo = /\s/;
+        var result = myRegex.test(address);
+        var resultOne = myRegexOne.test(address);
+        var resultTwo = myRegexTwo.test(address)
+        if (address.length < 5){
+            return document.getElementById("addressError").classList.add("no-error")
+        }
+        if (result!=true) {
+            return document.getElementById("addressError").classList.add("no-error")        
+        }
+        if (resultOne!=true) {
+            return document.getElementById("addressError").classList.add("no-error")        
+        }
+        if (resultTwo!=true) {
+            return document.getElementById("addressError").classList.add("no-error")        
         }
     }
-    return "Address: Have no letters";
-    }
-    console.log(checkAddressLetters(address))
-
-
-
-    function checkAddressSpace(a){
-    for(i=0; i<a.length; i++){
-        if (space.indexOf(a.charAt(i),0)!=-1){
-            return "Address: Have Space";
-        }
-    }
-    return "Address: Have no Space";
-    }
-    console.log(checkAddressSpace(address)); // Check split 
 
     // ***** check City / Town *****
-    // VARIABLE to city
-    var city = "Rosario";
-
+    var inputCity = document.getElementById("city");
+    inputCity.onblur = checkCity;
+    
     function checkCity (a) {
-        if(a.length<3){
-            return "City / Town: < 3 number";
+        var city = inputCity.value;
+        if(city.length < 3){
+            return document.getElementById("cityError").classList.remove("no-error")        
         }
-        return "City / Town: Ok"
     }
-    console.log(checkCity(city))
+    inputCity.onfocus = checkCityOne;
+
+    function checkCityOne (a) {
+        var city = inputCity.value;
+        if(city.length < 3){
+            return document.getElementById("cityError").classList.add("no-error")        
+        }
+    }
 
     // ***** check Postal Code *****
-    // VARIABLE to postalCode
-    var postalCode = "2000";
-
-    function checkPostalCode (a) {
-        if(a.length<3){
-            return "Postal Code: < 3 number";
+    var inputpostalCode = document.getElementById("postalCode");
+    inputpostalCode.onblur = checkPostalCode
+    function checkPostalCode () {
+        var postalCode = inputpostalCode.value
+        if(postalCode.length<3){
+            return document.getElementById("postalCodeError").classList.remove("no-error")        
         }
-        return "Postal Code: Ok"
     }
-    console.log(checkPostalCode(postalCode))
+    inputpostalCode.onfocus = checkPostalCodeOne
+    function checkPostalCodeOne () {
+        var postalCode = inputpostalCode.value
+        if(postalCode.length<3){
+            return document.getElementById("postalCodeError").classList.add("no-error")        
+        }
+    }
+
 
     // ***** check DNI *****
-    // VARIABLE to dni
-    var dni = "123456789";
-
+    var inputDni = document.getElementById("dni");
+    inputDni.onblur = checkDni
     function checkDni (a) {
-        if(a.length < 7){
-            return "DNI: < 7 number";
+        var dni = inputDni.value
+        if(dni.length < 7){
+            return document.getElementById("dniError").classList.remove("no-error")        
         }
-        if(a.length >8){
-            return "DNI: > 8 number";
+        if(dni.length >8){
+            return document.getElementById("dniError").classList.remove("no-error")        
         }
-        return "DNI: Ok"
     }
-    console.log(checkDni(dni))
-*/
+    inputDni.onfocus = checkDniOne
+    function checkDniOne (a) {
+        var dni = inputDni.value
+        if(dni.length < 7){
+            return document.getElementById("dniError").classList.add("no-error")        
+        }
+        if(dni.length >8){
+            return document.getElementById("dniError").classList.add("no-error")        
+        }
+    }
+
+    var inputbutton = document.getElementById("send")
+    inputbutton.onclick = sendInfo
+
+    function sendInfo (e){
+        e.preventDefault()
+        alert("Full Name: " + inputName.value + 
+        " Email:  " + inputEmail.value +
+        " Password: " + inputPassword.value +
+        " Repeat your password: " + inputRepeatPassword.value +
+        " Age: " + inputAge.value +
+        " Phone Number: " + inputPhoneNumber + 
+        " Address: " + inputAddress.value +
+        " City/Town: " + inputCity.value +
+        " Postal Code: " + inputpostalCode.value + 
+        " Dni: " + inputDni.value
+        )
+    }
+
+}
